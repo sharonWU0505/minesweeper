26d2ad5aae4e5c51c212a962324b36224708e7e3
sharonWU0505 <yasyuanwu@gmail.com>
2021-05-01 16:18:58 +0800

    chore: install react-95 for styling the app with windows style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from "react";
 import "./assets/scss/main.scss";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { styleReset } from "react95";
+// pick a theme of your choice
+import original from "react95/dist/themes/original";
+// original Windows95 font (optionally)
+import ms_sans_serif from "react95/dist/fonts/ms_sans_serif.woff2";
+import ms_sans_serif_bold from "react95/dist/fonts/ms_sans_serif_bold.woff2";
 import Game from "./containers/Game";
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif}') format('woff2');
+    font-weight: 400;
+    font-style: normal
+  }
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif_bold}') format('woff2');
+    font-weight: bold;
+    font-style: normal
+  }
+  body {
+    font-family: 'ms_sans_serif';
+  }
+  ${styleReset}
+`;
 
 function App() {
   return (
     <div>
-      <h1>Minesweeper</h1>
-      <Game />
+      <GlobalStyles />
+      <ThemeProvider theme={original}>
+        <div>
+          <h1>Minesweeper</h1>
+          <Game />
+        </div>
+      </ThemeProvider>
     </div>
   );
 }
