7c49d719bbaf52e53d14b8b3df013e67436db5fb
sharonWU0505 <yasyuanwu@gmail.com>
2021-05-01 19:31:35 +0800

    feat: style window with background color

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./assets/scss/main.scss";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
-import { styleReset } from "react95";
+import { styleReset, Window, WindowHeader } from "react95";
 // pick a theme of your choice
 import original from "react95/dist/themes/original";
 // original Windows95 font (optionally)
 import ms_sans_serif from "react95/dist/fonts/ms_sans_serif.woff2";
 import ms_sans_serif_bold from "react95/dist/fonts/ms_sans_serif_bold.woff2";
 import Game from "./containers/Game";
+import styled from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
   @font-face {
@@ -28,15 +29,30 @@ const GlobalStyles = createGlobalStyle`
   ${styleReset}
 `;
 
+const AppWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background: teal;
+  padding: 30px;
+`;
+
+const StyledWindow = styled(Window)`
+  width: fit-content;
+  display: block;
+  margin: 0 auto;
+`;
+
 function App() {
   return (
     <div>
       <GlobalStyles />
       <ThemeProvider theme={original}>
-        <div>
-          <h1>Minesweeper</h1>
-          <Game />
-        </div>
+        <AppWrapper>
+          <StyledWindow>
+            <WindowHeader>Minesweeper</WindowHeader>
+            <Game />
+          </StyledWindow>
+        </AppWrapper>
       </ThemeProvider>
     </div>
   );
